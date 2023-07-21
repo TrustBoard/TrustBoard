0c6057faf8e221a1782dbc3d776b7b3af7944bfc
pierreVinet <pierre.vinet17@gmail.com>
2023-07-21 21:38:39 +0200

    refactor: blank page with header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+"use-client";
+
+export default function Header() {
+  return (
+    <div className="w-full flex flex-row justify-between px-10">
+      <span className="text-3xl font-extrabold">TrustBoard</span>
+      <p>Profile</p>
+    </div>
+  );
+}
