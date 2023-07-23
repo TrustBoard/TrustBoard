3d2e607eebfa552602f31430838034da3a22e9d8
Arkoys <arkoys0427@gmail.com>
2023-07-23 06:50:25 +0200

    add: UI - TrustBoard Logo Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import ConnectWallet from "./ConnectWallet";
 import Profile from "./Profile";
 import SearchBar from "./SearchBar";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <div className="w-full absolute top-0 flex flex-row justify-between items-center px-10 py-10 bg-white">
-      <span className="text-3xl font-extrabold">TrustBoard</span>
+      <Image src={"/trustboard.svg"} alt="" width="200" height="120"/>
+      {/*<span className="text-3xl font-extrabold">TrustBoard</span>*/}
       <SearchBar />
       {/* <Profile /> */}
       {/* <ConnectMetamask /> */}
